627de00d212a5fd18fa4ea262308c890565466b8
christian <ceqocampo9@gmail.com>
2022-12-30 18:49:53 +0800

    Initial Commit

---
+++ awesome/user_variables.lua
@@ -0,0 +1,19 @@
+return {
+	widget = {
+		weather = {
+			--- API Key
+			key = "",
+			--- Coordinates
+			coordinates = {
+				"12.8797", --- lat
+				"121.7740", --- lon
+			},
+		},
+
+		--- Github activity
+		github = {
+			username = "ceqocampo9@gmail.com",
+			password = "Nov2022105605",
+		},
+	},
+}
